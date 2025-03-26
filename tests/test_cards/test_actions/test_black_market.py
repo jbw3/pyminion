bc9e141791866bf6ecf8abadd2dac5958a6f3777
John Wilkes <jwilkes0011@gmail.com>
2025-03-25 22:11:36 -0500

    Adding another black market test

--- tests/test_cards/test_actions/test_black_market.py
+++ tests/test_cards/test_actions/test_black_market.py
@@ -25,6 +25,7 @@ def test_black_market_buy(human: Human, game: Game, monkeypatch):
     assert len(black_market_deck) == 50
 
     top_card = black_market_deck.get_top()
+    other_top_card_names = set(c.name for c in black_market_deck.cards[-3:-1])
 
     human.hand.add(black_market)
     human.hand.add(copper)
@@ -44,3 +45,36 @@ def test_black_market_buy(human: Human, game: Game, monkeypatch):
     assert set(c.name for c in human.playmat) == {"Black Market", "Gold"}
     assert len(human.discard_pile) == 1
     assert human.discard_pile.cards[0].name == top_card.name
+    assert len(black_market_deck) == 49
+    bottom_card_names = set(c.name for c in black_market_deck.cards[:2])
+    assert other_top_card_names == bottom_card_names
+
+
+@pytest.mark.expansions([base_set, intrigue_set, promos_set])
+@pytest.mark.kingdom_cards([black_market])
+def test_black_market_no_buy(human: Human, game: Game, monkeypatch):
+    black_market_deck = game.get_non_supply_pile(BlackMarket.DECK_NAME)
+    assert len(black_market_deck) == 50
+
+    top_card_names = set(c.name for c in black_market_deck.cards[-3:])
+
+    human.hand.add(black_market)
+    human.hand.add(copper)
+    human.hand.add(gold)
+    human.hand.add(gold)
+
+    responses = ["gold,gold", "", ""]
+    monkeypatch.setattr("builtins.input", lambda _: responses.pop(0))
+
+    human.play(black_market, game)
+    assert len(responses) == 0
+    assert human.state.money == 8
+    assert human.state.buys == 1
+    assert len(human.hand) == 1
+    assert human.hand.cards[0].name == "Copper"
+    assert len(human.playmat) == 3
+    assert set(c.name for c in human.playmat) == {"Black Market", "Gold"}
+    assert len(human.discard_pile) == 0
+    assert len(black_market_deck) == 50
+    bottom_card_names = set(c.name for c in black_market_deck.cards[:3])
+    assert top_card_names == bottom_card_names
